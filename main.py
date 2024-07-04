6157290b9a707ba7fef116d787ad4d571dc5b862
Cyber Moose <cybermoose1989@gmail.com>
2024-07-04 18:00:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,16 @@
 ####
 ####
 
-print(5+5)
-
 a = float(input("Enter First Number: "))
 symbol = input("Enter Symbol (+, -, *, /): ")
 b = float(input("Enter Second Number: "))
 
-operations = {
+oper= {
     '+': a + b,
     '-': a - b,
     '*': a * b,
     '/': a / b if b != 0 else "Error! Division by zero."
 }
 
-result = operations.get(symbol, "Invalid symbol.")
+result = oper.get(symbol, "Invalid symbol.")
 print(f"The result is: {result}")
